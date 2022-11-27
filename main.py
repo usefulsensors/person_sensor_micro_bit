f39d54b35cc463d373323510535b3a034789651b
Pete Warden <pete@petewarden.com>
2022-11-27 15:38:57 -0800

    Added example code

---
+++ main.py
@@ -0,0 +1,64 @@
+# MicroPython example of reading the Person Sensor from Useful Sensors on a
+# Micro:bit. See https://bit.ly/ps_dev for full documentation on the module, and
+# README.md for details on wiring and assembly.
+
+from microbit import *
+
+import struct
+import time
+
+# The person sensor has the I2C ID of hex 62, or decimal 98.
+PERSON_SENSOR_I2C_ADDRESS = 0x62
+
+# We will be reading raw bytes over I2C, and we'll need to decode them into
+# data structures. These strings define the format used for the decoding, and
+# are derived from the layouts defined in the developer guide.
+PERSON_SENSOR_I2C_HEADER_FORMAT = "BBH"
+PERSON_SENSOR_I2C_HEADER_BYTE_COUNT = struct.calcsize(
+    PERSON_SENSOR_I2C_HEADER_FORMAT)
+
+PERSON_SENSOR_FACE_FORMAT = "BBBBBBbB"
+PERSON_SENSOR_FACE_BYTE_COUNT = struct.calcsize(PERSON_SENSOR_FACE_FORMAT)
+
+PERSON_SENSOR_FACE_MAX = 4
+PERSON_SENSOR_RESULT_FORMAT = PERSON_SENSOR_I2C_HEADER_FORMAT + \
+    "B" + PERSON_SENSOR_FACE_FORMAT * PERSON_SENSOR_FACE_MAX + "H"
+PERSON_SENSOR_RESULT_BYTE_COUNT = struct.calcsize(PERSON_SENSOR_RESULT_FORMAT)
+
+# How long to pause between sensor polls.
+PERSON_SENSOR_DELAY = 0.2
+
+# Keep looping and reading the person sensor results.
+while True:
+    read_data = i2c.read(PERSON_SENSOR_I2C_ADDRESS,
+                         PERSON_SENSOR_RESULT_BYTE_COUNT)
+
+    offset = 0
+    (pad1, pad2, payload_bytes) = struct.unpack_from(
+        PERSON_SENSOR_I2C_HEADER_FORMAT, read_data, offset)
+    offset = offset + PERSON_SENSOR_I2C_HEADER_BYTE_COUNT
+
+    (num_faces) = struct.unpack_from("B", read_data, offset)
+    num_faces = int(num_faces[0])
+    offset = offset + 1
+
+    faces = []
+    for i in range(num_faces):
+        (box_confidence, box_left, box_top, box_right, box_bottom, id_confidence, id,
+         is_facing) = struct.unpack_from(PERSON_SENSOR_FACE_FORMAT, read_data, offset)
+        offset = offset + PERSON_SENSOR_FACE_BYTE_COUNT
+        face = {
+            "box_confidence": box_confidence,
+            "box_left": box_left,
+            "box_top": box_top,
+            "box_right": box_right,
+            "box_bottom": box_bottom,
+            "id_confidence": id_confidence,
+            "id": id,
+            "is_facing": is_facing,
+        }
+        faces.append(face)
+    checksum = struct.unpack_from("H", read_data, offset)
+    print(num_faces, faces)
+
+    time.sleep(PERSON_SENSOR_DELAY)
